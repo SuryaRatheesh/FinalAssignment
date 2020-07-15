52517ac9ffa6d8f61923efb294c9c763164ecb33
SuryaRatheesh <65178099+SuryaRatheesh@users.noreply.github.com>
2020-07-15 10:56:39 +0530

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        id:1,
+        name: "Surya Ratheesh",
+        email: "surya@suyati.com"
+    },
+    {
+        id:2,
+        name: "George",
+        email: "ggeorge@suyati.com"
+    },
+    {
+        id:3,
+        name: "sreeraj",
+        email: "sreeraj@suyati.com"
+    },
+    {
+        id:4,
+        name: "Renjith",
+        email: "renjith@suyati.com"
+    }
+];
